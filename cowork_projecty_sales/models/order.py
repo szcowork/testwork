f92d937cd40e5bafa874e8f507be6ca3c8017071
Blue <cym@einfo-tech.com>
2020-04-20 11:54:02 +0800

    ps.purchase.requisition

--- cowork_projecty_sales/models/order.py
+++ cowork_projecty_sales/models/order.py
@@ -9,7 +9,7 @@ class sale_order(models.Model):
 
     analysis_id = fields.Many2one("cowork.project.availability.analysis",string=u'项目可行性分析')
 
-class purchase_order(models.Model):
-    _inherit = 'purchase.order'
+class ps_purchase_requisition(models.Model):
+    _inherit = 'ps.purchase.requisition'
 
     analysis_id = fields.Many2one("cowork.project.availability.analysis",string=u'项目可行性分析')
